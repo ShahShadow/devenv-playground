7d8557d66355067a6e51d9cf36fe481cc634c35f
Hemal Shah <hemalvarsha@gmail.com>
2019-06-17 16:11:52 -0700

    Add async version of server.

--- test-1/testapi-server.cc
+++ test-1/testapi-server.cc
@@ -16,11 +16,155 @@ class TestApiServerImpl final : public Greeter::Service {
     reply->set_message(FormatString("Hello %s", request->name()));
     return grpc::Status::OK;
   }
+
+  grpc::Status SayHelloStream(grpc::ServerContext* context, const HelloRequest* request,
+                  grpc::ServerWriter<HelloResponse>* writer) override {   
+    HelloResponse response;
+    response.set_message(FormatString("Hello %s", request->name()));
+    writer->Write(response);
+
+
+    response.set_message(FormatString("Hello AGAIN %s", request->name()));
+    writer->Write(response);
+    return grpc::Status::OK;
+  }
 };
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "Bringing up server" << std::endl;
+class TestApiAsyncServerImpl final {
+  public:
+  ~TestApiAsyncServerImpl() {
+    server_->Shutdown();
+    // Always shutdown the completion queue after the server.
+    cq_->Shutdown();
+  }
+
+
+// There is no shutdown handling in this code.
+  void Run() {
+    std::string server_address("0.0.0.0:50051");
+
+    grpc::ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service_" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *asynchronous* service.
+    builder.RegisterService(&service_);
+    // Get hold of the completion queue used for the asynchronous communication
+    // with the gRPC runtime.
+    cq_ = builder.AddCompletionQueue();
+    // Finally assemble the server.
+    server_ = builder.BuildAndStart();
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Proceed to the server's main loop.
+    HandleRpcs();
+  }
+
+ private:
+  // Class encompasing the state and logic needed to serve a request.
+  class CallData {
+   public:
+    // Take in the "service" instance (in this case representing an asynchronous
+    // server) and the completion queue "cq" used for asynchronous communication
+    // with the gRPC runtime.
+    CallData(Greeter::AsyncService* service, grpc::ServerCompletionQueue* cq)
+        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
+      // Invoke the serving logic right away.
+      Proceed();
+    }
+
+    void Proceed() {
+      if (status_ == CREATE) {
+        // Make this instance progress to the PROCESS state.
+        status_ = PROCESS;
+
+        // As part of the initial CREATE state, we *request* that the system
+        // start processing SayHello requests. In this request, "this" acts are
+        // the tag uniquely identifying the request (so that different CallData
+        // instances can serve different requests concurrently), in this case
+        // the memory address of this CallData instance.
+        service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_,
+                                  this);
+      } else if (status_ == PROCESS) {
+        // Spawn a new CallData instance to serve new clients while we process
+        // the one for this CallData. The instance will deallocate itself as
+        // part of its FINISH state.
+        new CallData(service_, cq_);
+
+        // The actual processing.
+        std::string prefix("Hello ");
+        reply_.set_message(prefix + request_.name());
+
+        // And we are done! Let the gRPC runtime know we've finished, using the
+        // memory address of this instance as the uniquely identifying tag for
+        // the event.
+        status_ = FINISH;
+        responder_.Finish(reply_, grpc::Status::OK, this);
+      } else {
+        GPR_ASSERT(status_ == FINISH);
+        // Once in the FINISH state, deallocate ourselves (CallData).
+        delete this;
+      }
+    }
+
+   private:
+    // The means of communication with the gRPC runtime for an asynchronous
+    // server.
+    Greeter::AsyncService* service_;
+
+    // The producer-consumer queue where for asynchronous server notifications.
+    grpc::ServerCompletionQueue* cq_;
+    // Context for the rpc, allowing to tweak aspects of it such as the use
+    // of compression, authentication, as well as to send metadata back to the
+    // client.
+    grpc::ServerContext ctx_;
+
+    // What we get from the client.
+    HelloRequest request_;
+    // What we send back to the client.
+    HelloResponse reply_;
+
+    // The means to get back to the client.
+    grpc::ServerAsyncResponseWriter<HelloResponse> responder_;
+
+    // Let's implement a tiny state machine with the following states.
+    enum CallStatus { CREATE, PROCESS, FINISH };
+    CallStatus status_;  // The current serving state.
+  };
+
+  // This can be run in multiple threads if needed.
+  void HandleRpcs() {
+    // Spawn a new CallData instance to serve new clients.
+    new CallData(&service_, cq_.get());
+    void* tag;  // uniquely identifies a request.
+    bool ok;
+    while (true) {
+      // Block waiting to read the next event from the completion queue. The
+      // event is uniquely identified by its tag, which in this case is the
+      // memory address of a CallData instance.
+      // The return value of Next should always be checked. This return value
+      // tells us whether there is any kind of event or cq_ is shutting down.
+      GPR_ASSERT(cq_->Next(&tag, &ok));
+      GPR_ASSERT(ok);
+      static_cast<CallData*>(tag)->Proceed();
+    }
+  }
+
+  std::unique_ptr<grpc::ServerCompletionQueue> cq_;
+  Greeter::AsyncService service_;
+  std::unique_ptr<grpc::Server> server_;
+};
+
+
+int RunAsync() {
+  std::cout << "Bringing up async server" << std::endl;
+	TestApiAsyncServerImpl server;
+  server.Run();
+  return 0;
+}
+
+int RunSync() {
+  std::cout << "Bringing up server" << std::endl;
 	 std::string server_address("0.0.0.0:50051");
   TestApiServerImpl service;
 
@@ -37,5 +181,10 @@ int main(int argc, char const *argv[])
   // Wait for the server to shutdown. Note that some other thread must be
   // responsible for shutting down the server for this call to ever return.
   server->Wait();
-	return 0;
+  return 0;
+}
+
+int main(int argc, char const *argv[])
+{   
+	return RunSync();
 }
